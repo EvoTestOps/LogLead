968f5d727ced74dd06b34a2f154e55f63275381a
mmantyla <mika.mantyla@helsinki.fi>
2023-10-09 05:34:20 +0000

    SVM and IsolationForrest added

--- anomaly_detection.py
+++ anomaly_detection.py
@@ -1,10 +1,15 @@
 import polars as pl
+import numpy as np
 from collections import Counter
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import LinearSVC
+from sklearn.ensemble import IsolationForest
+
 from sklearn.metrics import accuracy_score
 from scipy.sparse import hstack
+import scipy.sparse
 
 
 class EventAnomalyDetection:
@@ -57,7 +62,7 @@ class SeqAnomalyDetection:
         self.numeric_cols = numeric_cols if numeric_cols else []
         #self.events, self.labels, self.additional_features = self._prepare_data(self.df_train)
 
-    def _prepare_data(self, df):
+    def __prepare_data(self, df):
         # Extract events
         events = []
         if self.event_col:
@@ -89,31 +94,66 @@ class SeqAnomalyDetection:
         
         return X, labels
     
-    def train_LR(self, df_seq):
-        X_train, labels = self.sklearn_vectorization(train=True, df_seq=df_seq)
-        self.lr = LogisticRegression(max_iter=1000)
-        self.lr.fit(X_train, labels)
-        
-    def predict_LR(self, df_seq, print_scores=True):
-        X_test, labels = self.sklearn_vectorization(train=False, df_seq=df_seq)
-        predictions = self.lr.predict(X_test)
-        df_seq = df_seq.with_columns(pl.Series(name="pred_normal", values=predictions.tolist()))
-        if print_scores:
-            self._print_evaluation_scores(labels, predictions, self.lr)
-        return df_seq
-    
-    def train_DT(self, df_seq):
-        X_train, labels = self.sklearn_vectorization(train=True, df_seq=df_seq)
-        self.dt = tree.DecisionTreeClassifier()
-        self.dt.fit(X_train, labels)
+    def _prepare_data(self, train, df_seq):
+        X = None
+        labels = df_seq.select(pl.col("normal")).to_series().to_list()
 
-    def predict_DT(self, df_seq, print_scores=True):
-        X_test, labels = self.sklearn_vectorization(train=False, df_seq=df_seq)
-        predictions = self.dt.predict(X_test)
+        #Extract events
+        if self.event_col:
+            events = df_seq.select(pl.col(self.event_col)).to_series().to_list()
+            events = [' '.join(e) for e in events]
+            #We are training
+            if train:
+                self.vectorizer = CountVectorizer()
+                X = self.vectorizer.fit_transform(events)
+            #We are predicting
+            else:
+                X = self.vectorizer.transform(events)
+
+        # Extract additional predictors
+        if self.numeric_cols:
+            additional_features = df_seq.select(self.numeric_cols).to_pandas().values
+            X = hstack([X, additional_features]) if X is not None else additional_features
+
+        return X, labels
+
+    #Overwrites previous model
+    #One can also give own model 
+    def train_model(self, df_seq, model):
+        X_train, labels = self._prepare_data(train=True, df_seq=df_seq)
+        self.model = model
+        #Isolation forrest cant handel csc_matrix 
+        #if isinstance(model, IsolationForest) and hasattr(X_train, 'tocsr'):
+        #    X_train = X_train.tocsr()
+        if isinstance(model, IsolationForest):
+            X_train = X_train.tocsr() if scipy.sparse.issparse(X_train) else X_train
+
+        self.model.fit(X_train, labels)
+        
+    def predict(self, df_seq, print_scores=True):
+        X_test, labels = self._prepare_data(train=False, df_seq=df_seq)
+        predictions = self.model.predict(X_test)
+        #IsolationForrest does not give binary predictions. Convert
+        if isinstance(self.model, IsolationForest):
+            predictions = np.where(predictions > 0, 1, 0)
         df_seq = df_seq.with_columns(pl.Series(name="pred_normal", values=predictions.tolist()))
         if print_scores:
-            self._print_evaluation_scores(labels, predictions, self.dt)
+            self._print_evaluation_scores(labels, predictions, self.model)
         return df_seq
+
+    def train_LR(self, df_seq):
+        self.train_model (df_seq, LogisticRegression(max_iter=1000))
+        
+    def train_DT(self, df_seq):
+        self.train_model (df_seq, DecisionTreeClassifier())
+
+    def train_SVM(self, df_seq,  penalty='l1', tol=0.1, C=1, dual=False, class_weight=None, max_iter=100):
+        self.train_model (df_seq, LinearSVC(
+            penalty=penalty, tol=tol, C=C, dual=dual, class_weight=class_weight, max_iter=max_iter))
+
+    def train_IsolationForest(self, df_seq, n_estimators=100, max_samples='auto', contamination=0.03):
+        self.train_model (df_seq, IsolationForest(
+            n_estimators=n_estimators, max_samples=max_samples, contamination=contamination))
 
     def _print_evaluation_scores(self, y_test, y_pred, model):
         # Evaluate the model's performance
@@ -143,17 +183,16 @@ class SeqAnomalyDetection:
             event_features = []
 
         all_features = event_features + self.numeric_cols
-        if isinstance(model, LogisticRegression):
+        if isinstance(model, (LogisticRegression, LinearSVC)):
             feature_importance = abs(model.coef_[0])
-        elif isinstance(model, tree.DecisionTreeClassifier):
+        elif isinstance(model, DecisionTreeClassifier):
             feature_importance = model.feature_importances_
         else:
-            raise ValueError("Model type not supported for feature importance extraction")
+            print("Model type not supported for feature importance extraction")
+            return
         sorted_idx = feature_importance.argsort()[::-1]  # Sort in descending order
 
         print("\nTop Important Predictors:")
         for i in range(min(10, len(sorted_idx))):  # Print top 10 or fewer
             print(f"{all_features[sorted_idx[i]]}: {feature_importance[sorted_idx[i]]:.4f}")
-
-
             
