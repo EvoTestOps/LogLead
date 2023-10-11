d746df9901fc8134419b34c6239accea2b0e8a1a
jnyyssol <jessetius@gmail.com>
2023-10-11 06:09:42 +0000

    Trying lot of stuff on trigram, loader for GELF

---
+++ loglead_demo_gelf_log.py
@@ -0,0 +1,20 @@
+#
+#Separate demo files
+import loader as load, enricher as er, anomaly_detection as ad
+
+
+gelf_processor = load.GELFLoader(filename="gelf.log")
+df_gelf = gelf_processor.execute()
+
+#Quick stuff to test:
+
+enricher_gelf = er.EventLogEnricher(df_gelf)
+df_gelf = enricher_gelf.words()
+df_gelf = enricher_gelf.alphanumerics()
+df_gelf = enricher_gelf.trigrams()
+df_gelf = enricher_gelf.parse_drain()
+
+event_anomaly_detection = ad.EventAnomalyDetection(df_gelf)
+df_gelf = event_anomaly_detection.compute_ano_score("e_words", 100)
+df_gelf = event_anomaly_detection.compute_ano_score("e_alphanumerics", 100)
+df_gelf = event_anomaly_detection.compute_ano_score("e_cgrams", 100)
