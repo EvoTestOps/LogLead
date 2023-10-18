d02160e269c11bbbe9d59eedd80a2278ecac66cb
yuqwang <yuqing.wang@helsinki.fi>
2023-10-18 10:57:41 +0000

    merge bert embedding classes into one

--- enricher.py
+++ enricher.py
@@ -1,9 +1,9 @@
 import polars as pl
 import drain3 as dr
-from basebert_embedding import BertEmbeddings
+from bertembedding import BertEmbeddings
 
-#Drain.ini default regexes 
-#No lookahead or lookbedinde so reimplemented with capture groups
+# Drain.ini default regexes
+# No lookahead or lookbedinde so reimplemented with capture groups
 masking_patterns_drain = [
     ("${start}<ID>${end}", r"(?P<start>[^A-Za-z0-9]|^)(([0-9a-f]{2,}:){3,}([0-9a-f]{2,}))(?P<end>[^A-Za-z0-9]|$)"),
     ("${start}<IP>${end}", r"(?P<start>[^A-Za-z0-9]|^)(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?P<end>[^A-Za-z0-9]|$)"),
@@ -13,7 +13,6 @@ masking_patterns_drain = [
     ("${start}<NUM>${end}", r"(?P<start>[^A-Za-z0-9]|^)([\-\+]?\d+)(?P<end>[^A-Za-z0-9]|$)"),
     ("${cmd}<CMD>", r"(?P<cmd>executed cmd )(\".+?\")")
 ]
-
 
 
 class EventLogEnricher:
@@ -46,27 +45,26 @@ class EventLogEnricher:
         return self.df
 
     # Function-based enricher to create trigrams from messages
-    #Trigrams enrichment is slow 1M lines in 40s.
-    #Trigram flag to be removed after this is fixed. 
-    #https://github.com/pola-rs/polars/issues/10833
-    #https://github.com/pola-rs/polars/issues/10890   
-    def trigrams(self, column="m_message"):
-        self._handle_prerequisites([column])
+    # Trigrams enrichment is slow 1M lines in 40s.
+    # Trigram flag to be removed after this is fixed.
+    # https://github.com/pola-rs/polars/issues/10833
+    # https://github.com/pola-rs/polars/issues/10890
+    def trigrams(self):
+        self._handle_prerequisites(["m_message"])
         if "e_cgrams" not in self.df.columns:
             self.df = self.df.with_columns(
-                pl.col(column).map_elements(
+                pl.col("m_message").map_elements(
                     lambda mes: self._create_cngram(message=mes, ngram=3)).alias("e_cgrams")
             )
         return self.df
 
-
     def _create_cngram(self, message, ngram=3):
         if ngram <= 0:
             return []
-        return [message[i:i+ngram] for i in range(len(message) - ngram + 1)]
-    
-    #Enrich with drain parsing results
-    def parse_drain(self, drain_masking=False, reparse = False):
+        return [message[i:i + ngram] for i in range(len(message) - ngram + 1)]
+
+    # Enrich with drain parsing results
+    def parse_drain(self, drain_masking=False, reparse=False):
         self._handle_prerequisites(["m_message"])
         if reparse or "e_event_id" not in self.df.columns:
             # Drain returns dict
@@ -75,89 +73,111 @@ class EventLogEnricher:
             # 'cluster_size': 2,
             # 'template_mined': 'session closed for user root',
             # 'cluster_count': 1}
-           #we store template for later use.
-            
-            # We might have multiline log message, i.e. log_message + stack trace. 
-            #Use only first line of log message for parsing
+            # we store template for later use.
+
+            # We might have multiline log message, i.e. log_message + stack trace.
+            # Use only first line of log message for parsing
             if drain_masking:
                 dr.template_miner.config_filename = 'drain3.ini'
                 self.tm = dr.TemplateMiner()
                 self.df = self.df.with_columns(
-                    message_trimmed = pl.col("m_message").str.split("\n").list.first()
+                    message_trimmed=pl.col("m_message").str.split("\n").list.first()
                 )
-                self.df = self.df.with_columns(drain = pl.col("message_trimmed").map_elements(lambda x: self.tm.add_log_message(x)))
+                self.df = self.df.with_columns(
+                    drain=pl.col("message_trimmed").map_elements(lambda x: self.tm.add_log_message(x)))
             else:
                 if "e_message_normalized" not in self.df.columns:
                     self.normalize()
                 dr.template_miner.config_filename = 'drain3_no_masking.ini'
                 self.tm = dr.TemplateMiner()
-                self.df = self.df.with_columns(drain = pl.col("e_message_normalized").map_elements(lambda x: self.tm.add_log_message(x)))
-     
+                self.df = self.df.with_columns(
+                    drain=pl.col("e_message_normalized").map_elements(lambda x: self.tm.add_log_message(x)))
+
             self.df = self.df.with_columns(
-                e_event_id=pl.lit("e") + pl.col("drain").struct.field("cluster_id").cast(pl.Utf8),#extra thing ensure we e1 e2 instead of 1 2
-                e_template = pl.col("drain").struct.field("template_mined"))
-            self.df = self.df.drop("drain")#Drop the dictionary produced by drain. Event_id and template are the most important. 
-            
-            #tm.drain.print_tree()
+                e_event_id=pl.lit("e") + pl.col("drain").struct.field("cluster_id").cast(pl.Utf8),
+                # extra thing ensure we e1 e2 instead of 1 2
+                e_template=pl.col("drain").struct.field("template_mined"))
+            self.df = self.df.drop(
+                "drain")  # Drop the dictionary produced by drain. Event_id and template are the most important.
+
+            # tm.drain.print_tree()
 
         return self.df
 
-    def create_neural_emb(self, emb_col="e_bert_emb"):
+    def create_neural_emb(self):
         self._handle_prerequisites(["m_message"])
-        if emb_col not in self.df.columns:
+        if "e_bert_emb" not in self.df.columns:
+            # We might have multiline log message, i.e. log_message + stack trace.
+            # Use only first line of log message for parsing
+
+            #            self.df = self.df.with_columns(
+            #                message_trimmed=pl.col("m_message").str.split("\n").list.first()
+            #                .str.to_lowercase()
+            #                .str.replace_all(r"[0-9\W_]", " ")
+            #                .str.replace_all(r"\s+", " ")
+            #                .str.replace_all(r"^\s+|\s+$", "")
+            #            )
             if "e_message_normalized" not in self.df.columns:
-                    self.normalize()
-            self.bert_emb_gen = BertEmbeddings()
-            #Feed the whole model as once to save compute
+                self.normalize()
+            # create a BertEmbeddings class instance
+            # MM: Do we need the generator later?
+            # For example if predictions need to be done much after training
+            # e.g. we train and then go to production where prediction take place.
+            # if so then this should be
+            # YQ: It depends on the situation. E.g., for my fusion model, I define it in '__main__',
+            # to save the processing time, i.e., define one time and use it in all places
+            self.bert_emb_gen = BertEmbeddings(bertmodel="albert")
+            # bert_emb_gen = BertEmbeddings()
+            # obtain bert sentence embedding
+            # MM: Is it possible to do this map or map_elements as in line 70?
+            # MM: Or is there too much performance hit?
+            # MM: This makes unnessary copies from and to polars dataframe.
+            # YQ: map or map_elements do the operation on "each element" of a column in the DataFrame.
+            # YQ: We can create Bert embedding for "each element" and map it to a column, but it takes heavy computing resources
+            # YQ: Because it fetch model and generate output again and again
+            # YQ: You can see from the current bertembedding class, I feed all values on self.df['message_trimmed'] to Bert
+            # YQ: This means we only fetch model and generate output one time.
             message_trimmed_list = self.df['e_message_normalized'].to_list()
             message_trimmed_emb_tensor = self.bert_emb_gen.create_bert_emb(message_trimmed_list)
             # Convert the eager tensor to a NumPy array
             message_trimmed_emb_list = message_trimmed_emb_tensor.numpy()
             bert_emb_col_df = pl.DataFrame({
-                emb_col: message_trimmed_emb_list
+                'e_bert_emb': message_trimmed_emb_list
             })
 
             self.df = self.df.hstack(bert_emb_col_df)
-            #print(self.df["e_bert_emb"][1])
-            #Albert, b_hadoop, 177592 entries, Time taken: 375.30 seconds
-            #Base Bert, b_hadoop, 177592 entries, Time taken: 601.45 seconds
-        else:
-            print(f"Column {emb_col} allready exists, drop the column or choose another name")
+            # print(self.df["e_bert_emb"][1])
+            # Albert, b_hadoop, 177592 entries, Time taken: 375.30 seconds
+            # Base Bert, b_hadoop, 177592 entries, Time taken: 601.45 seconds
         return self.df
 
     def length(self):
         self._handle_prerequisites(["m_message"])
         if "e_message_len" not in self.df.columns:
             self.df = self.df.with_columns(
-                e_message_len_char = pl.col("m_message").str.n_chars(),
-                e_message_len_lines = pl.col("m_message").str.count_matches(r"(\n|\r|\r\n)")
+                e_message_len_char=pl.col("m_message").str.n_chars(),
+                e_message_len_lines=pl.col("m_message").str.count_matches(r"(\n|\r|\r\n)")
             )
         return self.df
-    
-    def normalize(self, regexs=masking_patterns_drain, to_lower =False):
-                
-        #base_code = 'self.df = self.df.with_columns(e_message_normalized = pl.col("m_message").str.split("\\n").list.first()'
+
+    def normalize(self, regexs=masking_patterns_drain, to_lower=False):
+
+        # base_code = 'self.df = self.df.with_columns(e_message_normalized = pl.col("m_message").str.split("\\n").list.first()'
         base_code = 'self.df.with_columns(e_message_normalized = pl.col("m_message").str.split("\\n").list.first()'
-      
+
         if to_lower:
             base_code += '.str.to_lowercase()'
-                
+
         # Generate the replace_all chain
         for key, pattern in regexs:
             replace_code = f'.str.replace_all(r"{pattern}", "{key}")'
             base_code += replace_code
-    
+
         base_code += ')'
         self.df = eval(base_code)
         return self.df
-        #print (base_code)
-        #return base_code    
-        
-
-
-
-
-
+        # print (base_code)
+        # return base_code
 
 
 class SequenceEnricher:
@@ -181,10 +201,10 @@ class SequenceEnricher:
         # Join this result with df_sequences on seq_id
         self.df_sequences = self.df_sequences.join(df_temp, on='seq_id')
         return self.df_sequences
-    
+
     def events(self):
         # Aggregate event ids into a list for each seq_id
-        df_temp = self.df.group_by('seq_id').agg(events = pl.col('e_event_id'))
+        df_temp = self.df.group_by('seq_id').agg(events=pl.col('e_event_id'))
         # Join this result with df_sequences on seq_id
         self.df_sequences = self.df_sequences.join(df_temp, on='seq_id')
         return self.df_sequences
@@ -195,7 +215,6 @@ class SequenceEnricher:
         self.df_sequences = self.df_sequences.join(df_temp, on='seq_id')
         return self.df_sequences
 
-
     def duration(self):
         # Calculate the sequence duration for each seq_id as the difference between max and min timestamps
         df_temp = self.df.group_by('seq_id').agg(
@@ -205,16 +224,16 @@ class SequenceEnricher:
         # Join this result with df_sequences on seq_id
         self.df_sequences = self.df_sequences.join(df_temp, on='seq_id')
         return self.df_sequences
-        
+
     def eve_len(self):
         # Count the number of rows for each seq_id
         df_temp = self.df.group_by('seq_id').agg(
-            eve_len_max = pl.col('e_message_len_char').max(),
-            eve_len_min = pl.col('e_message_len_char').min(),
-            eve_len_avg = pl.col('e_message_len_char').mean(),
-            eve_len_med = pl.col('e_message_len_char').median(),
-            eve_len_over1 = (pl.col('e_message_len_char') > 1).sum()
-            )
+            eve_len_max=pl.col('e_message_len_char').max(),
+            eve_len_min=pl.col('e_message_len_char').min(),
+            eve_len_avg=pl.col('e_message_len_char').mean(),
+            eve_len_med=pl.col('e_message_len_char').median(),
+            eve_len_over1=(pl.col('e_message_len_char') > 1).sum()
+        )
         # Join this result with df_sequences on seq_id
         self.df_sequences = self.df_sequences.join(df_temp, on='seq_id')
         return self.df_sequences    
