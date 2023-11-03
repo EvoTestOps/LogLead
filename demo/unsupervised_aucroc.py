dd372369b05e812f5c640f5b5e741a488da62139
jnyyssol <jessetius@gmail.com>
2023-11-03 10:46:08 +0000

    oov analyzer for split, aucroc for unsupervised

---
+++ demo/unsupervised_aucroc.py
@@ -0,0 +1,77 @@
+#
+#Separate demo files
+import sys
+sys.path.append('..')
+# Suppress ConvergenceWarning
+import warnings
+from sklearn.exceptions import ConvergenceWarning
+warnings.filterwarnings("ignore", category=ConvergenceWarning)
+
+import loglead.loader as load, loglead.enricher as er, loglead.anomaly_detection as ad
+import polars as pl
+
+full_data = "/home/ubuntu/Datasets"
+
+
+loader = load.BGLLoader(filename=f"{full_data}/bgl/BGL.log")
+df = loader.execute()
+df = loader.reduce_dataframes(frac=0.05)
+df = df.filter(pl.col("m_message").is_not_null())
+enricher =  er.EventLogEnricher(df)
+
+regexs = [('0','\d'),('0','0+')]
+df = enricher.normalize(regexs, to_lower=True)
+df = enricher.trigrams("e_message_normalized")
+
+sad =  ad.SupervisedAnomalyDetection(item_list_col="e_cgrams")
+sad.test_train_split (df, test_frac=0.7, vec_name="TfidfVectorizer",oov_analysis=True)
+sad.evaluate_all_ads(disabled_methods=["train_LOF", "train_OneClassSVM","train_LR","train_DT","train_LSVM","train_RF","train_XGB"])
+
+
+
+
+loader = load.HadoopLoader(filename=f"{full_data}/hadoop/",
+                                            filename_pattern  ="*.log",
+                                            labels_file_name=f"{full_data}/hadoop/abnormal_label_accurate.txt")
+df = loader.execute()
+df = loader.reduce_dataframes(frac=1)
+df_seq = loader.df_sequences       
+
+df = df.filter(pl.col("m_message").is_not_null())
+enricher =  er.EventLogEnricher(df)
+
+regexs = [('0','\d'),('0','0+')]
+df = enricher.normalize(regexs, to_lower=True)
+df = enricher.trigrams("e_message_normalized")
+
+
+seq_enricher = er.SequenceEnricher(df = df, df_sequences = df_seq)
+seq_enricher.tokens(token="e_cgrams")
+sad =  ad.SupervisedAnomalyDetection(item_list_col="e_cgrams")
+sad.test_train_split (seq_enricher.df_sequences, test_frac=0.7, vec_name="TfidfVectorizer",oov_analysis=True)
+print(sad.test_df)
+sad.evaluate_all_ads(disabled_methods=["train_LOF", "train_OneClassSVM","train_LR","train_DT","train_LSVM","train_RF","train_XGB"])
+
+
+
+
+loader = load.HDFSLoader(filename=f"{full_data}/hdfs/HDFS.log", 
+                                    labels_file_name=f"{full_data}/hdfs/anomaly_label.csv")
+df = loader.execute()
+df = loader.reduce_dataframes(frac=0.05)
+df_seq = loader.df_sequences       
+
+df = df.filter(pl.col("m_message").is_not_null())
+enricher =  er.EventLogEnricher(df)
+
+regexs = [('0','\d'),('0','0+')]
+df = enricher.normalize(regexs, to_lower=True)
+df = enricher.trigrams("e_message_normalized")
+
+
+seq_enricher = er.SequenceEnricher(df = df, df_sequences = df_seq)
+seq_enricher.tokens(token="e_cgrams")
+sad =  ad.SupervisedAnomalyDetection(item_list_col="e_cgrams")
+sad.test_train_split (seq_enricher.df_sequences, test_frac=0.7, vec_name="TfidfVectorizer",oov_analysis=True)
+print(sad.test_df)
+sad.evaluate_all_ads(disabled_methods=["train_LOF", "train_OneClassSVM","train_LR","train_DT","train_LSVM","train_RF","train_XGB"])
