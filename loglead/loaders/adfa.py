b72a4cad42a604cfeb81205d948a1cd0fcdd280b
jnyyssol <jessetius@gmail.com>
2024-05-08 13:39:05 +0300

    loaders for adfa and awsctd

---
+++ loglead/loaders/adfa.py
@@ -0,0 +1,63 @@
+from loglead.loaders.base import BaseLoader
+import polars as pl
+from glob import glob
+import os
+
+
+class ADFALoader(BaseLoader):
+    """
+    Note, that this dataset already consists of event IDs, so further enhancing is not required.
+    Here the "filename" needs to point to the directory called "ADFA-LD" that can be extracted from the zip in:
+    https://github.com/verazuo/a-labelled-version-of-the-ADFA-LD-dataset
+
+    """
+    
+    def __init__(self, filename, df=None, df_seq=None):
+        super().__init__(filename, df, df_seq)
+        self._mandatory_columns = ["m_message"]
+
+
+    def load(self):
+        m_messages = []
+        seq_ids = []
+        labels = []
+
+        logfiles = [y for x in os.walk(self.filename) for y in glob(os.path.join(x[0], '*.txt'))]
+        print(logfiles)
+        for logfile in logfiles:
+            if 'ADFA-LD+Syscall+List.txt' in logfile:
+                continue  # Skip label file
+
+            with open(logfile) as logsource:
+                logfile_parts = logfile.strip('\n').split('/')
+                
+                # Determine label based on directory naming
+                if 'Attack_Data_Master' in logfile:
+                    label = '_'.join(logfile_parts[3].split('_')[:-1])
+                else:
+                    label = 'Normal'
+
+                # Use file name as sequence identifier
+                seq_id = logfile_parts[-1].replace('.txt', '')
+                line_nr = 1
+                for line in logsource:
+                    for event_id in line.strip('\n ').split(' '):
+                        m_messages.append(event_id)
+                        seq_ids.append(seq_id)
+                        labels.append(label)
+                        line_nr += 1
+
+        self.df = pl.DataFrame({
+            'm_message': m_messages,
+            'seq_id': seq_ids,
+            'label': labels
+        })
+
+    def preprocess(self):
+        # Here the sequence based dataframe is created
+        self.df_seq = self.df.with_columns(
+            (pl.col('label') != "Normal").alias('is_anomaly')
+        ).groupby('seq_id').agg([
+            pl.col('m_message'),  # Aggregates all 'm_message' into a list
+            pl.any('is_anomaly').alias('anomaly')  # Checks if there's any label not equal to 'Normal'
+        ])
---
+++ loglead/loaders/awsctd.py
@@ -0,0 +1,56 @@
+from loglead.loaders.base import BaseLoader
+import polars as pl
+from glob import glob
+import os
+
+class AWSCTDLoader(BaseLoader):
+    """
+    Note, that this dataset already consists of event IDs, so further enhancing is not required.
+    Here the "filename" needs to point to the directory called "CSV" that can be extracted from the 7z-file in:
+    https://github.com/DjPasco/AWSCTD
+    """
+
+    def __init__(self, filename, df=None, df_seq=None):
+        super().__init__(filename, df, df_seq)
+        self._mandatory_columns = ["m_message"]
+
+          
+    def load(self):
+        m_messages = []
+        seq_ids = []
+        labels = []
+
+        logfiles = [y for x in os.walk(self.filename) for y in glob(os.path.join(x[0], '*.csv'))]
+        for logfile in logfiles:
+            with open(logfile) as logsource:
+                logfile_parts = logfile.strip('\n').split('/')
+                line_nr = 1
+                for line in logsource:
+                    line_parts = line.strip('\n').split(',')
+                    label = line_parts[-1]
+                    if label == "Clean":
+                        label = "Normal"
+                    seq_id = logfile_parts[-2] + '/' + logfile_parts[-1].replace('.csv', '') + '_' + str(line_nr)  # Use filename + incrementing id per sequence
+
+                    for event_id in line_parts[:-1]:
+                        # Append data to lists
+                        m_messages.append(event_id)
+                        seq_ids.append(seq_id)
+                        labels.append(label)
+
+                    line_nr += 1
+
+        self.df = pl.DataFrame({
+            'm_message': m_messages,
+            'seq_id': seq_ids,
+            'label': labels
+        })
+
+    def preprocess(self):
+        # Here the sequence based dataframe is created
+        self.df_seq = self.df.with_columns(
+            (pl.col('label') != "Normal").alias('is_anomaly')
+        ).groupby('seq_id').agg([
+            pl.col('m_message'),  # Aggregates all 'event_type' into a list
+            pl.any('is_anomaly').alias('anomaly')  # Checks if there's any label not equal to 'Normal'
+        ])
