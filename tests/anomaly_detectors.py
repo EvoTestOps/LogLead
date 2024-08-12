27b60edc8da010fd078695181db8eaed928abd4f
jnyyssol <jessetius@gmail.com>
2024-08-12 08:22:30 +0000

    refactored tests/anomaly_detectors.py

--- tests/anomaly_detectors.py
+++ tests/anomaly_detectors.py
@@ -24,6 +24,7 @@ all_files = glob.glob(os.path.join(test_data_path, "*.parquet"))
 
 print("Anomaly detectors test starting.")
 print(f"Found files {all_files}")
+
 # Extract unique dataset names with '_eh_' , excluding '_seq' files
 datasets = set()
 for f in all_files:
@@ -34,8 +35,35 @@ for f in all_files:
 
 cols_event = ["m_message", "e_words", "e_event_drain_id", "e_trigrams", "e_event_tip_id", "e_event_lenma_id", "e_bert_emb"] 
 numeric_cols = ["seq_len", "eve_len_max", "duration_sec", "eve_len_over1", "nep_prob_nmax_avg", "nep_prob_nmax_min"]
+
+def run_anomaly_detectors(df, cols_event, numeric_cols, test_frac):
+    for col in cols_event:
+        disabled_methods = set()
+        disabled_methods.add("train_LOF")  # Too slow. Disabled to make tests run faster
+        disabled_methods.add("train_OneClassSVM")  # Too slow. Disabled to make tests run faster
+        if col == "m_message":
+            disabled_methods.add("train_OOVDetector")
+        if ((col == "e_trigrams" or col == "m_message") and "nezha_tt" in df.columns):
+            disabled_methods.add("train_RF")  # Disable this slow combo.
+        if col in df.columns and "anomaly" in df.columns and df["normal"].sum() > 9 and df["anomaly"].sum() > 9:
+            print(f"Running anomaly detectors with {col}")
+            sad = AnomalyDetector(item_list_col=col, print_scores=False, store_scores=True)
+            sad.test_train_split(df, test_frac=test_frac) 
+            sad.evaluate_all_ads(disabled_methods=disabled_methods)
+        else:
+            print (f"Skipped column {col}. Missing, no anomaly label or too low number (<10) of normal ({df_seq['normal'].sum()}) or anomaly ({df_seq['anomaly'].sum()}) instances")    
+
+
+    # Because this requires all the determined columns, it inherently only runs with sequenced dfs
+    all_columns_exist = all(column in df.columns for column in numeric_cols)
+    if "anomaly" in df.columns and df["normal"].sum() > 9 and df["anomaly"].sum() > 9 and all_columns_exist:
+        print(f"Running seqeuence anomaly detectors with numeric columns {numeric_cols}")
+        disabled_methods = {"train_RarityModel", "train_OOVDetector"}
+        sad = AnomalyDetector(numeric_cols=numeric_cols, print_scores=False, store_scores=True)
+        sad.test_train_split(df, test_frac=test_frac) 
+        sad.evaluate_all_ads(disabled_methods=disabled_methods)
+
 for dataset in datasets:
-    # Check if ad should be skipped for this dataset
     dataset_config = next((d for d in config['datasets'] if d['name'] in dataset), None)
 
     if dataset_config and not dataset_config.get('anomaly_detection', True):
@@ -44,61 +72,15 @@ for dataset in datasets:
 
     # Load the event level data
     primary_file = os.path.join(test_data_path, f"{dataset}.parquet")
-    print(f"\nLoading {primary_file}")
-    df = pl.read_parquet(primary_file)
-
-    for col in cols_event:
-        disabled_methods = set()
-        disabled_methods.add("train_LOF") #Too slow. Disabled to make tests run faster
-        disabled_methods.add("train_OneClassSVM") #Too slow. Disabled to make tests run faster
-        if col == "m_message":
-            disabled_methods.add("train_OOVDetector")
-          #Only run if the predictor column AND anomaly labels are present AND we have at least two classes in the data
-        if ((col == "e_trigrams" or col == "m_message") and "nezha_tt" in dataset):
-            disabled_methods.add("train_RF") #Disable this slow combo. 
-        if (col in df.columns and "anomaly" in df.columns and 
-        df["normal"].sum() > 10 and
-        df["anomaly"].sum() > 10):
-            print(f"Running event anomaly detectors with {col}")
-            sad = AnomalyDetector(item_list_col=col, print_scores= False, store_scores=True)
-            sad.test_train_split (df, test_frac=0.5) 
-            sad.evaluate_all_ads(disabled_methods=disabled_methods)
-
-
     seq_file = primary_file.replace(f"{dataset}.parquet", f"{dataset}_seq.parquet")
+
     if os.path.exists(seq_file):
         df_seq = pl.read_parquet(seq_file)
-        print(f"Running seqeuence anomaly detectors with {seq_file}")
-        for col in cols_event:
-            disabled_methods = set()
-            disabled_methods.add("train_LOF") #Too slow. Disabled to make tests run faster
-            disabled_methods.add("train_OneClassSVM") #Too slow. Disabled to make tests run faster
-            if col == "m_message":
-                disabled_methods.add("train_OOVDetector")
-                #Only run if the predictor column AND anomaly labels are present AND we have at least two classes in the data
-            if (col in df_seq.columns and "anomaly" in df_seq.columns and 
-            df_seq["normal"].sum() > 9 and
-            df_seq["anomaly"].sum() > 9):
-                print(f"Running seqeuence anomaly detectors with {col}")
-                sad =  AnomalyDetector(item_list_col=col, print_scores= False, store_scores=True)
-                #High training fraction to ensure we always have suffiecient samples as these are reduced dataframes 
-                sad.test_train_split (df_seq, test_frac=0.2) 
-                sad.evaluate_all_ads(disabled_methods=disabled_methods)
-            else: 
-                print (f"Column {col} missing or too low number (<11) of normal sequences { df_seq['normal'].sum()} or anomaly sequences {df_seq['anomaly'].sum()}")    
-        # Check that numeric columns exist
-        all_columns_exist = all(column in df_seq.columns for column in numeric_cols)
-        if ("anomaly" in df_seq.columns and 
-        df_seq["normal"].sum() > 9 and
-        df_seq["anomaly"].sum() > 9 and 
-        all_columns_exist):
-            print(f"Running seqeuence anomaly detectors with numeric columns {numeric_cols}")
-            disabled_methods = set()
-            disabled_methods.add("train_RarityModel")
-            disabled_methods.add("train_OOVDetector")
-            sad = AnomalyDetector(numeric_cols = numeric_cols, print_scores= False, store_scores=True)
-            #High training fraction to ensure we always have suffiecient samples as these are reduced dataframes 
-            sad.test_train_split (df_seq, test_frac=0.2) 
-            sad.evaluate_all_ads(disabled_methods=disabled_methods)
+        print(f"Running sequence anomaly detectors with {seq_file}")
+        run_anomaly_detectors(df_seq, cols_event, numeric_cols, test_frac=0.2)
+    else:
+        df = pl.read_parquet(primary_file)
+        print(f"Running event anomaly detectors with {seq_file}")
+        run_anomaly_detectors(df, cols_event, numeric_cols, test_frac=0.5)
 
 print("Anomaly detectors test complete.")
