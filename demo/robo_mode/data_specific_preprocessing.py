16e675033ac3f0c7d8c6b8e36f0e156b73ed4b13
mmantyla <mika.mantyla@gmail.com>
2024-09-18 17:40:43 +0300

    cleanup robomode preprocessing

--- demo/robo_mode/data_specific_preprocessing.py
+++ demo/robo_mode/data_specific_preprocessing.py
@@ -23,7 +23,7 @@ def remove_run_name_from_file_names(df):
     """
     Adjusts the container log file names in a Hadoop environment by removing 
     the application ID part from the file names. This is necessary for making 
-    file names consistent across different runs, enabling a fair comparison.
+    file names consistent across different runs, enabling comparison.
     
     For example, in a run with application ID 'application_1445062781478_0011':
     
@@ -33,24 +33,12 @@ def remove_run_name_from_file_names(df):
     This renaming ensures that log file names match across all runs after preprocessing.
     """
     # Extract the <Common_part> by removing the 'application' prefix from 'run'
-    #OLD WAY
-#    df = df.with_columns([
-#        pl.col("run").str.strip_prefix("application").alias("common_part")
-#    ])
     # Remove all chars before number. My_run_123_2 has common part 123_2
     df = df.with_columns(
         pl.col("run").str.replace_all(r"^[^\d]+", "").alias("common_part")
     )
-
-    print (df)
-
     print(f"Running hadoop preprocessing remove_run_name_from_file_names")
     
-    # Dynamic replacement not yet supported in polars
-    #df = df.with_columns([
-    #    pl.col("file_name").str.replace(pl.col("common_part"), "")
-    #])
-
     # Get unique <Common_part> values
     unique_parts = df["common_part"].unique()
     # Loop over unique <Common_part> values to replace them in the 'file_name' column
@@ -58,8 +46,7 @@ def remove_run_name_from_file_names(df):
         df = df.with_columns([
             pl.col("file_name").str.replace(part, "").alias("file_name")
         ])
-    
-    print (df)
+
     df = df.drop("common_part")
 
     return df
