707240313b16e0b2eb1dfe55fe29c8085ca6095b
jnyyssol <jessetius@gmail.com>
2024-12-03 10:12:12 +0200

    added lo2 to loader init

--- loglead/loaders/__init__.py
+++ loglead/loaders/__init__.py
@@ -9,6 +9,7 @@ from .nezha import NezhaLoader
 from .pro import ProLoader
 from .supercomputers import ThuSpiLibLoader
 from .raw import RawLoader
+from .lo2 import LO2Loader
 
 __all__ = ['ADFALoader', 'AWSCTDLoader', 'BGLLoader', 'GELFLoader', 'HadoopLoader', 'HDFSLoader', 'NezhaLoader',
-           'ProLoader', 'ThuSpiLibLoader', 'BaseLoader', 'RawLoader']
+           'ProLoader', 'ThuSpiLibLoader', 'BaseLoader', 'RawLoader', 'LO2Loader']
