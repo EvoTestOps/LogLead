7dd3166b4968e75675a76c2c8923dff06b5535ef
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2025-09-04 11:24:19 +0300

    Option for Drain persistence mode

--- loglead/parsers/drain3/drain.py
+++ loglead/parsers/drain3/drain.py
@@ -1,9 +1,11 @@
 import os.path
 
 from drain3 import TemplateMiner
+from drain3.file_persistence import FilePersistence
 from drain3.template_miner_config import TemplateMinerConfig
 
-__all__ = ['DrainTemplateMiner', 'DrainTemplateMinerNoMasking']
+__all__ = ['DrainTemplateMiner', 'DrainTemplateMinerNoMasking',
+           'DrainPersistenceTemplateMiner', 'DrainPersistenceTemplateMinerNoMasking']
 
 current_script_path = os.path.abspath(__file__)
 current_script_directory = os.path.dirname(current_script_path)
@@ -12,7 +14,14 @@ tmc = TemplateMinerConfig()
 tmc.load(ini_location)
 DrainTemplateMiner = TemplateMiner(config=tmc)
 
+persistence = FilePersistence('drain3_state.json')
+DrainPersistenceTemplateMiner = TemplateMiner(persistence, config=tmc)
+
+
 ini_location = os.path.join(current_script_directory, 'drain3_no_masking.ini')
 tmc = TemplateMinerConfig()
 tmc.load(ini_location)
 DrainTemplateMinerNoMasking = TemplateMiner(config=tmc)
+
+persistence_no_masking = FilePersistence('drain3_state_no_masking.json')
+DrainPersistenceTemplateMinerNoMasking = TemplateMiner(persistence_no_masking, config=tmc)
